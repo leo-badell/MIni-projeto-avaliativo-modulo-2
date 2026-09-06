d8cb7dd1974690bbd3e8a9bdfe8dcd371cd94ab9
leonardobadell@protonmail.com <leonardobadell@protonmail.com>
2026-09-06 22:02:42 +0000

    Feat/Inicio do repositório e configuração de ambiente

---
+++ Pipeline-de-processamento-clássico.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Sprint 1 - Confirguração e versionamento
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Sprint 2 - Análise exploratória clássica (OpenCV)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Sprint 3 - Destaque de características
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Sprint 4 - Ingestão de dados e augmentation (Keras)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Sprint 5 - A arquitetura CNN e treinamento
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Sprint 6 - Auditoria e gravação
